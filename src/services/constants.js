ac6bb4b579f1d8fed5ce5a0606757c70d946f3fa
konez2k <32417933+konez2k@users.noreply.github.com>
2019-06-25 01:33:19 +0200

    Update constants.js

--- src/services/constants.js
+++ src/services/constants.js
@@ -1,1 +1,1 @@
-export const apiEndpoint = 'http://webwallet.bitg.org/api/v1'; // Leave it as a temporary URL for test, BITG is not affiliated with MB.
+export const apiEndpoint = 'https://webwallet.bitg.org/api/v1'; // Leave it as a temporary URL for test, BITG is not affiliated with MB.
